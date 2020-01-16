f5d917c793d7982f7bcb1a3b9870a761a9097608
Martin Petrov <mkpetrov@abv.bg>
2020-01-16 18:59:22 +0200

    Add project files.

---
+++ TestWebApp/Models/MainGridModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TestWebApp.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class MainGridModel
+    {
+        public string FootballMatch { get; set; }
+        public string HomeWinningPercent { get; set; }
+        public string DrawWinningPercent { get; set; }
+        public string AwayWinningPercent { get; set; }
+    }
+}
